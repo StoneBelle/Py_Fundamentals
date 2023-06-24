91e40325e1a5bdbd153594631f3cb4a2e56e96b7
Stone <65476500+StoneBelle@users.noreply.github.com>
2023-06-24 11:58:54 -0400

    Add files via upload

--- scrabble.py
+++ scrabble.py
@@ -1,21 +1,62 @@
 
 letters = ["A", "B", "C", "D", "E", "F", "G", "H",
 "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
-"S", "T", "U", "V", "W", "X", "Y", "Z"]
+"S", "T", "U", "V", "W", "X", "Y", "Z", " "]
 
 
-points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10, 0]
 
 
 
 # Create a point dict 
 
+letter_points = {}
 
 for x in range(27):
-    print(x)
+    letter_points[letters[x]] = points[x]
+
+
     
 
-# Score a word 
+# Determine score of a word
+
+def score_word(word):
+
+    point_total = 0 
+
+    for ch in word.upper():
+        if ch in letter_points:
+            point_total += letter_points[ch]    
+
+    return point_total
+
+print(score_word("cat"))
 
 
-# Score a game
+
+
+
+# Create  a dict of a player's word history
+p_words = {
+    "player1" : ["BLUE", "TENNIS", "EXIT"],
+    "wordNerd" : ["EARTH", "EYES", "MACHINE"],
+    "Lexi Con" : ["ERASER", "BELLY", "HUSKY"],
+    "Prof Reader" : ["ZAP", "COMA", "PERIOD"]
+}
+
+
+# Q10 - Empty Dict to store player's points
+player_to_points = {}
+
+ # 
+for player, words in p_words.items():
+    p_points = 0
+    for word in words:
+        p_points += score_word(word)
+    player_to_points[player] = p_points
+    
+  
+print(player_to_points)
+
+
+# print(score_word("MACHINE"))
